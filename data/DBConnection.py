0bf7526909f133ad45a7a436a0fb9c3a70f5037c
Mickyas Tesfamichael <mickyastesfamichael@gmail.com>
2019-04-03 18:52:17 +0300

    added two more methods in DBConnection.py

--- data/DBConnection.py
+++ data/DBConnection.py
@@ -1,8 +1,10 @@
 import sqlite3
 import os
+from data import Member
 
 
 class DBConnection:
+
     __PATH_TO_DB = "database/data.db"
     if not os.path.exists(__PATH_TO_DB):
         os.mkdir("database")
@@ -12,14 +14,14 @@ class DBConnection:
 
     @staticmethod
     def add_member(member):
-        member_data_tuple = (
+        member_data = (
             member.first_name,
             member.last_name,
-            member.hasDuplicateFirstName,
-            member.canBeStage,
-            member.canRotateMic,
-            member.canAssist2ndHall,
-            member.sundayException
+            member.has_sunday_exception,
+            member.can_be_stage,
+            member.can_rotate_mic,
+            member.can_assist_2nd_hall,
+            member.has_sunday_exception
         )
 
         query = "INSERT INTO member_info " \
@@ -30,33 +32,74 @@ class DBConnection:
                 "canRotateMic," \
                 "canAssist2ndHall," \
                 "sundayException) VALUES " \
-                "(?, ?, ?, ?, ?, ?, ?)"
-        DBConnection.cursor.execute(query, member_data_tuple)
+                "(?, ?, ?, ?, ?, ?, ?);"
+        DBConnection.cursor.execute(query, member_data)
+        DBConnection.__con.commit()
 
     @staticmethod
     def remove_member(member):
-        query = "DELETE FROM member_info WHERE member_info.id = ?"
+        query = "DELETE FROM member_info WHERE member_info.id = ?;"
         DBConnection.cursor.execute(query, member.ID)
+        DBConnection.__con.commit()
 
     @staticmethod
     def update_member(member):
-        member_data_tuple = (
+        member_data = (
             member.first_name,
             member.last_name,
-            member.hasDuplicateFirstName,
-            member.canBeStage,
-            member.canRotateMic,
-            member.canAssist2ndHall,
-            member.sundayException,
+            member.has_sunday_exception,
+            member.can_be_stage,
+            member.can_rotate_mic,
+            member.can_assist_2nd_hall,
+            member.has_sunday_exception,
             member.ID
         )
 
-        query = "UPDATE member_info SET" \
+        query = "UPDATE member_info SET " \
                 "firstName = ?," \
                 "lastName = ?," \
-                "hasDuplicateFirstName = ?" \
+                "hasDuplicateFirstName = ?," \
                 "canBeStage = ?," \
                 "canRotateMic = ?," \
                 "canAssist2ndHall = ?," \
-                "sundayException = ? WHERE member_info.id = ?"
-        DBConnection.cursor.execute(query, member_data_tuple)
+                "sundayException = ? WHERE member_info.id = ?;"
+        DBConnection.cursor.execute(query, member_data)
+        DBConnection.__con.commit()
+
+    # getting a single or all members may be necessary sometimes
+    @staticmethod
+    def get_member(member_id):
+        id_tuple = (member_id,)
+        query = "SELECT * FROM member_info WHERE member_info.id = ?;"
+
+        member_info = DBConnection.cursor.execute(query, id_tuple).fetchone()
+        member = Member.Member()
+        member.ID = member_info[0]
+        member.first_name = member_info[1]
+        member.last_name = member_info[2]
+        member.first_name_is_duplicate = member_info[3]
+        member.can_be_stage = member_info[4]
+        member.can_rotate_mic = member_info[5]
+        member.can_assist_2nd_hall = member_info[6]
+        member.has_sunday_exception = member_info[7]
+
+        return member
+
+    @staticmethod
+    def get_all_members():
+        query = "SELECT * FROM member_info;"
+        all_members = []
+
+        for member_info in DBConnection.cursor.execute(query).fetchall():
+            member = Member.Member()
+            member.ID = member_info[0]
+            member.first_name = member_info[1]
+            member.last_name = member_info[2]
+            member.first_name_is_duplicate = member_info[3]
+            member.can_be_stage = member_info[4]
+            member.can_rotate_mic = member_info[5]
+            member.can_assist_2nd_hall = member_info[6]
+            member.has_sunday_exception = member_info[7]
+            all_members.append(member)
+
+        return all_members
--- data/Member.py
+++ data/Member.py
@@ -6,8 +6,8 @@ class Member:
         self.ID = 0
         self.first_name = ""
         self.last_name = ""
-        self.hasDuplicateFirstName = False
-        self.canBeStage = True
-        self.canRotateMic = True
-        self.canAssist2ndHall = True
-        self.sundayException = False
+        self.first_name_is_duplicate = False
+        self.can_be_stage = True
+        self.can_rotate_mic = True
+        self.can_assist_2nd_hall = True
+        self.has_sunday_exception = False
